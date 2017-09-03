1af7d65bceceeb5d9e6aa0ee30e4a23065852b15
Yubo Zhi <normanzyb@gmail.com>
2017-09-03 20:44:16 +0100

    Calculate sum of differences between raw sequences

--- misc/rawdiff.c
+++ misc/rawdiff.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
+#include <math.h>
 
 static const uint8_t header[6] = {0xde, 0xad, 0xbe, 0xef, 0x02, 0x00};
 
@@ -34,29 +35,37 @@ int main(int argc, char *argv[])
 		return 2;
 	int ia = avg;
 	unsigned long err = 0ul;
+	unsigned long total = 0ul, totalErr = 0ul, totalSumErr = 0ul;
 	for (unsigned long i = 0;; i++) {
 		long ca = readFrame(a, pa);
 		long cb = readFrame(b, pb);
 		if (ca < 0)
-			fprintf(stderr, "File A exit with %ld\n", ca);
+			fprintf(stderr, "File A exited with %ld\n", ca);
 		if (cb < 0)
-			fprintf(stderr, "File B exit with %ld\n", ca);
+			fprintf(stderr, "File B exited with %ld\n", ca);
 		if (ca < 0 || cb < 0)
 			break;
+		total += ca >= cb ? ca : cb;
 		if (ca != cb) {
-			err = ca >= cb ? ca - cb : cb - ca;
+			err = labs(ca - cb);
 			ca = ca >= cb ? cb : ca;
 		}
 		uint8_t *qa = pa, *qb = pb;
-		while (ca--)
+		unsigned long e = 0ul;
+		while (ca--) {
+			totalSumErr += abs((int)*qa - (int)*qb);
 			if (*qa++ != *qb++)
-				err++;
+				e++;
+		}
+		err += e;
+		totalErr += e;
 		if (--ia == 0) {
 			printf("%lu,%lu\n", i, err);
 			err = 0ul;
 			ia = avg;
 		}
 	}
+	fprintf(stderr, "%lu,%lu,%lu\n", total, totalErr, totalSumErr);
 	free(pa);
 	free(pb);
 	fclose(a);
