133f8858632bc9a755b77a99c66eb2f2e901243b
Yubo Zhi <normanzyb@gmail.com>
2017-11-22 22:19:00 +0000

    Command for building codec.exe

--- ffmpeg/build_codec.bat
+++ ffmpeg/build_codec.bat
@@ -1,1 +1,5 @@
-cl /Ox /LD codec.c /D WIN32 /I "D:\Programs\misc\Multimedia\ffmpeg-20170824-f0f4888-win64-shared\include" /I "..\external\pthread-w32\include" /I "..\external\fmod\inc" /D HAVE_STRUCT_TIMESPEC /D ENABLE_FMOD /link /LIBPATH:"D:\Programs\misc\Multimedia\ffmpeg-20170824-f0f4888-win64-shared\lib" avcodec.lib avformat.lib avutil.lib swscale.lib avfilter.lib /LIBPATH:"..\external\fmod\lib" fmod64_vc.lib /LIBPATH:"..\external\pthread-w32\lib\x64" pthreadVC2.lib
+@rem codec.dll
+cl /Ox /LD codec.c /D WIN32 /I "D:\Programs\misc\Multimedia\ffmpeg-20170824-f0f4888-win64\include" /I "..\external\pthread-w32\include" /I "..\external\fmod\inc" /D HAVE_STRUCT_TIMESPEC /D ENABLE_FMOD /link /LIBPATH:"D:\Programs\misc\Multimedia\ffmpeg-20170824-f0f4888-win64\lib" avcodec.lib avformat.lib avutil.lib swscale.lib avfilter.lib /LIBPATH:"..\external\fmod\lib" fmod64_vc.lib /LIBPATH:"..\external\pthread-w32\lib\x64" pthreadVC2.lib
+
+@rem codec.exe
+csc /out:codec.exe /target:exe codec.cs
